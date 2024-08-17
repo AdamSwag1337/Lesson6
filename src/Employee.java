7cee1e87ec84ee87a976c9d1aaf125a6cb1bcb19
AdamSwag1337 <artem.podchernyaev@gmail.com>
2024-08-17 20:23:37 +0300

    Try1

---
+++ src/Employee.java
@@ -0,0 +1,18 @@
+public class Employee {
+    private String fio;
+    private String position;
+    private String email;
+    private int phone;
+    private double salary;
+    private int age;
+
+    public Employee(String fio, String position, String email, int phone, double salary, int age){
+        this.fio=fio;
+        this.position=position;
+        this.email=email;
+        this.phone=phone;
+        this.salary=salary;
+        this.age=age;
+    }
+
+}
